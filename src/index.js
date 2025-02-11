0cc575d05bd7c3d8e9d55cd03f6c947e5a70fb17
Michael Mumme <neptica@hotmail.com>
2025-02-10 22:37:47 -0600

    add board, ship drag and drop, and snap to grid
    add functionality to refuse poor ship placement

--- src/index.js
+++ src/index.js
@@ -1,9 +1,79 @@
 import Player from "./player.js";
+import { shipGUI } from "./shipGUI.js";
 import "./css/style.css";
 
 (function () {
-  console.log("Hello");
-  const h1 = document.createElement("h1");
-  h1.textContent = "HELLO";
-  document.appendChild(h1);
+  const menu = document.getElementById("main__menu");
+  const msgContainer = document.getElementById("message__container");
+
+  const board = document.getElementById("board__container");
+  initializeBoard(board);
+
+  const aside = document.getElementById("options__container");
+
+  const ships = initializeShips(board);
+  ships.forEach(({ ship, freeze }) => aside.appendChild(ship));
+
+  const button = document.getElementById("confirm");
+  button.addEventListener("click", () => {
+    const allPlaced = getShipPositions(board);
+    if (allPlaced) {
+      freezeShips(ships);
+      msgContainer.removeChild(button);
+    }
+  });
+  button.textContent = "Confirm Ship positions";
+  button.style.height = "50px";
+  button.style.width = "350px";
+  msgContainer.appendChild(button);
 })();
+
+function getShipPositions(grid) {
+  let shipCoords = [];
+  const shipElements = grid.querySelectorAll(".items div");
+
+  if (shipElements.length != 5) {
+    return false;
+  }
+
+  const gridItems = grid.querySelectorAll(".items");
+  for (let i = 0; i < shipElements.length; i++) {
+    for (let j = 0; j < gridItems.length; j++) {
+      if (gridItems[j] === shipElements[i].parentElement) {
+        console.log(
+          shipElements[i].dataset.rotation,
+          Math.floor(j / 10),
+          j % 10,
+        );
+        shipCoords.push([
+          shipElements[i].dataset.rotation,
+          Math.floor(j),
+          j % 10,
+        ]);
+      }
+    }
+  }
+  return true;
+}
+
+function initializeBoard(grid) {
+  for (let i = 0; i < 100; i++) {
+    const item = document.createElement("div");
+    item.classList.add("items");
+    grid.appendChild(item);
+  }
+}
+
+function initializeShips(board) {
+  let battleshipGUI = shipGUI();
+  const ship = battleshipGUI.battleShip(document, board, 5);
+  const ship1 = battleshipGUI.battleShip(document, board, 4);
+  const ship2 = battleshipGUI.battleShip(document, board, 3);
+  const ship3 = battleshipGUI.battleShip(document, board, 3);
+  const ship4 = battleshipGUI.battleShip(document, board, 2);
+  return [ship, ship1, ship2, ship3, ship4];
+}
+
+function freezeShips(ships) {
+  ships.forEach(({ ship, freeze }) => freeze());
+}
---
+++ src/shipGUI.js
@@ -0,0 +1,175 @@
+export { shipGUI };
+function shipGUI() {
+  function battleShip(keyGrabContainer, noFloatContainer, length) {
+    const ship = document.createElement("div");
+    let coordX;
+    let coordY;
+
+    ship.classList.add("ships");
+    ship.dataset.length = length;
+    ship.dataset.rotation = "horizontal";
+    ship.addEventListener("mouseup", move);
+
+    // Box Setup
+    let gap = 4;
+    const tiles = document.getElementsByClassName("items");
+    let locationStyle = tiles[0].getBoundingClientRect();
+    ship.style.height = locationStyle.height - gap + "px";
+    ship.style.width =
+      locationStyle.height +
+      (length - 1) * (locationStyle.height + gap) -
+      gap +
+      "px";
+
+    function move() {
+      ship.style.position = "fixed";
+      let oldBox = ship.getBoundingClientRect();
+      if (coordX === undefined) {
+        coordX = oldBox.x;
+        coordY = oldBox.y;
+      }
+
+      ship.removeEventListener("mouseup", move);
+      ship.addEventListener("mouseup", unSelect);
+      keyGrabContainer.addEventListener("mousemove", drag);
+      keyGrabContainer.addEventListener("keydown", rotate);
+    }
+
+    function rotate(event) {
+      if (event.code == "KeyR") {
+        let oldBox = ship.getBoundingClientRect();
+        let newWidth = oldBox.height;
+        let newHeight = oldBox.width;
+
+        if (newWidth < newHeight) {
+          ship.dataset.rotation = "vertical";
+        } else {
+          ship.dataset.rotation = "horizontal";
+        }
+
+        ship.style.height = newHeight + "px";
+        ship.style.width = newWidth + "px";
+      }
+    }
+
+    function drag(event) {
+      const underMouse = document.elementsFromPoint(
+        event.clientX,
+        event.clientY,
+      );
+      let tile = -1;
+      let grid = -1;
+      for (let i = 0; i < underMouse.length; ++i) {
+        if (underMouse[i].classList.contains("items")) {
+          tile = underMouse[i];
+        } else if (underMouse[i].id == noFloatContainer.id) {
+          grid = underMouse[i];
+        }
+      }
+
+      if (tile != -1) {
+        let selected = tile.getBoundingClientRect();
+        let boxStyle = ship.getBoundingClientRect();
+        let padding;
+        if (boxStyle.height < boxStyle.width) {
+          padding = (selected.height - boxStyle.height) / 2;
+        } else {
+          padding = (selected.width - boxStyle.width) / 2;
+        }
+        let checkX = selected.x + padding;
+        let checkY = selected.y + padding;
+        ship.style.left = checkX + "px";
+        ship.style.top = checkY + "px";
+      } else if (grid == -1) {
+        ship.style.left = event.clientX + "px";
+        ship.style.top = event.clientY + "px";
+      }
+
+      return ship;
+    }
+
+    function unSelect(event) {
+      ship.addEventListener("mouseup", move);
+      ship.removeEventListener("mouseup", unSelect);
+      keyGrabContainer.removeEventListener("mousemove", drag);
+      keyGrabContainer.removeEventListener("keydown", rotate);
+
+      const underMouse = document.elementsFromPoint(
+        event.clientX,
+        event.clientY,
+      );
+      let tile = -1;
+      for (let i = 0; i < underMouse.length; ++i) {
+        if (underMouse[i].classList.contains("items")) {
+          tile = underMouse[i];
+        }
+      }
+
+      if (tile != -1) {
+        let boxStyle = ship.getBoundingClientRect();
+        tile.appendChild(ship);
+        ship.style.position = "absolute";
+        coordX = boxStyle.x;
+        coordY = boxStyle.y;
+      } else {
+        if (ship.parentElement.id != "options__container") {
+          ship.style.left = coordX + "px";
+          ship.style.top = coordY + "px";
+          ship.style.position = "absolute";
+        } else {
+          ship.style.position = "static";
+        }
+      }
+    }
+
+    function freeze() {
+      ship.removeEventListener("mouseup", move);
+      keyGrabContainer.removeEventListener("mousemove", drag);
+      keyGrabContainer.removeEventListener("keydown", rotate);
+    }
+    return { ship, freeze };
+  }
+
+  function adjustShips() {
+    const tiles = document.getElementsByClassName("items");
+    let locationStyle = tiles[0].getBoundingClientRect();
+    const ships = document.getElementsByClassName("ships");
+
+    let gap = 4;
+    for (let i = 0; i < ships.length; i++) {
+      // Resize
+      if (ships[i].dataset.rotation == "horizontal") {
+        ships[i].style.height = locationStyle.height - gap + "px";
+        ships[i].style.width =
+          locationStyle.height +
+          (ships[i].dataset.length - 1) * (locationStyle.height + gap) -
+          gap +
+          "px";
+      } else if (ships[i].dataset.rotation == "vertical") {
+        ships[i].style.height =
+          locationStyle.height +
+          (ships[i].dataset.length - 1) * (locationStyle.height + gap) -
+          gap +
+          "px";
+        ships[i].style.width = locationStyle.height - gap + "px";
+      }
+
+      // Reposition
+      let selected = ships[i].parentElement.getBoundingClientRect();
+      let boxStyle = ships[i].getBoundingClientRect();
+      let padding;
+      if (boxStyle.height < boxStyle.width) {
+        padding = (selected.height - boxStyle.height) / 2;
+      } else {
+        padding = (selected.width - boxStyle.width) / 2;
+      }
+      let checkX = selected.x + padding;
+      let checkY = selected.y + padding;
+      ships[i].style.left = checkX + "px";
+      ships[i].style.top = checkY + "px";
+    }
+  }
+
+  window.addEventListener("resize", adjustShips);
+  return { battleShip, adjustShips };
+}
